60b9be71c205f2d7f706a7c7f30e5841e2180524
Maria <zhmasha2505@gmail.com>
2022-10-25 15:35:24 +0300

    second commit

--- main.js
+++ main.js
@@ -1,41 +1,33 @@
-// const hoursElement = document.getElementById("hours");
-// const minutesElement = document.getElementById("minutes");
-// const secondsElement = document.getElementById("seconds");
-// const ampmElement = document.getElementById("ampm");
-
-// function updateClock() {
-//     let hour = new Date().getHours();
-//     let minute = new Date().getMinutes();
-//     let second = new Date().getSecond();
-//     let ampm = "AM";
-//     if (hour > 12) {
-//         hour = hour - 12;
-//         ampm = "PM";
-//     }
-
-//     hoursElement.innerHTML = hour;
-// }
-
-// updateClock();
-
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 const ampmEl = document.getElementById("ampm");
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSecond();
-let ampm = "AM";
-
 function updateClock() {
+    //Setting variables
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+    //Changing PM and AM
     if (h > 12) {
         h = h - 12;
         ampm = "PM";
-    };
-    hoursEl.innerHTML = h;
+    }
+    //Two digit format
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    //Synchronizing with html file
+    hoursEl.innerText = h;
+    minutesEl.innerText = m;
+    secondsEl.innerText = s;
+    ampmEl.innerText = ampm;
+    //Updating the function every second
+    setTimeout(() => {
+        updateClock();
+    }, 1000)
 }
-
 updateClock();
 
 
